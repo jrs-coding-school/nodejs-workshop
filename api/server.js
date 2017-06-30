61836c1890524d96416196cde33940969b9ab3d8
Tom Wilson <tom@jackhq.com>
2017-06-30 14:54:13 -0400

    update branch

--- api/server.js
+++ api/server.js
@@ -9,21 +9,39 @@ const db = PouchDB(process.env.DB)
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const fetch = require('isomorphic-fetch')
+const distributor = require('./distributor')
+
+async function changes() {
+  const res = await fetch(
+    `${process.env.DB}/_changes?feed=longpoll&since=now&include_docs=true`
+  ).then(res => res.json())
+  if (res.results.length > 0) {
+    distributor(res.results[0].doc)
+  }
+  changes()
+}
+changes()
 
 app.use(cors({ credentials: true, origin: 'http://localhost:5000' }))
 
+// app.get('/csv', async (req, res) => {
+//   // const src = request(process.env.DB + '/_all_docs?include_docs=true', {
+//   //   json: true
+//   // })
+//   const src = pullFetch.json(process.env.DB + '/_all_docs?include_docs=true')
+//   const sink = res
+//
+//   pull(src, stringify(), toPull.sink(sink))
+//
+//   //map(x => x + 1, [1,2,3,4,5])
+//   //#> 2,3,4,5,6
+// })
+
 app.get('/movies', async (req, res) => {
-  const fetchData = compose(
-    map(s =>
-      fetch(`${process.env.OMDB_API}&s=${s}`)
-        .then(res => res.json())
-        .then(res => res.Search)
-    ),
-    map(trim),
-    split(',')
-  )
-  const data = await Promise.all(fetchData(req.query.s))
-  res.send({ Response: 'True', Search: concat(...data) })
+  const data = await fetch(
+    `${process.env.OMDB_API}&s=${req.query.s}`
+  ).then(res => res.json())
+  res.send(data)
 })
 
 app.post('/favorites', bodyParser.json(), async (req, res) => {
