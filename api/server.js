353851cafc046622ed382437fa6272b72f641b2d
Tom Wilson <tom@jackhq.com>
2017-06-30 11:19:08 -0400

    complete

--- api/server.js
+++ api/server.js
@@ -1,19 +1,36 @@
 process.env.NODE_ENV !== 'production' && require('dotenv').config()
 const express = require('express')
 const app = express()
-const { pluck } = require('ramda')
+const { pluck, split } = require('ramda')
 
 const PouchDB = require('pouchdb-http')
 PouchDB.plugin(require('pouchdb-find'))
 const db = PouchDB(process.env.DB)
-
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const fetch = require('isomorphic-fetch')
 
 app.use(cors({ credentials: true, origin: 'http://localhost:5000' }))
 
+app.get('/movies', async (req, res) => {
+  try {
+    const data = await fetch(
+      `${process.env.OMDB_API}&s=${req.query.s}`
+    ).then(res => res.json())
+
+    if (data.Response === 'False') {
+      return res.status(500).send({ message: 'Movie Not Found!' })
+    }
+    res.send(data)
+  } catch (err) {
+    res.status(500).send({ ok: false })
+  }
+})
+
 app.post('/favorites', bodyParser.json(), async (req, res) => {
   try {
+    // TODO: joi doc validator
+    // const validdoc = await joi(rules, req.body)
     const result = await db.post(req.body)
     const doc = await db.get(result.id)
     res.send(doc)
